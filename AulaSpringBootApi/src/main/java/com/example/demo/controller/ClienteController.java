2f705b23bf8f4ad2014013ea35b6ab0f7c34a162
Stefano <stefano_bluyus@hotmail.com>
2025-10-14 18:46:48 -0300

    feat: ajustando o projeto para funcionar com MySQL + criação de busca por cliente

--- AulaSpringBootApi/src/main/java/com/example/demo/controller/ClienteController.java
+++ AulaSpringBootApi/src/main/java/com/example/demo/controller/ClienteController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,16 @@ public class ClienteController {
 	public List<Cliente> listar()
 	{
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping(params = "id")
+	public ResponseEntity<Cliente> getCliente(@RequestParam Long id) {
+	    var cliente = clienteRepository.findById(id);
+	    
+	    if (cliente.isPresent())
+	        return ResponseEntity.ok(cliente.get());
+	    else
+	        return ResponseEntity.notFound().build();
 	}
 	
 	@PostMapping // Quando houver um post nesse controller, esse método irá responder
